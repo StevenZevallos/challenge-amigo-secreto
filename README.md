bd6dafb270d2a1d1e74a4b3b2db827bd70b96a48
Steven <stevenzevallossalcedo@gmail.com>
2025-01-29 13:34:14 -0500

    challenge terminado

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 <h1>Instrucciones</h1>
+
+Se agregaron funciones como agregarAmigo(), sortearAmigo()
+
+donde en la función agregarAmigo, tenemos variables, y condicionales, como el if, para hacer que ingresen un nombre valido; por otro lado tenemos nuestra variable lista donde se obtiene los elementos de la lista agregados.
+
+en la funcion sortearAmigo(), hacemos lo mismo con una condicion if, para que no se pueda sortear cuando no haya nombres, te saldra un alert, tenemos nuestra variable para sortear el nombre, y por ultimo nuestro variable donde te dira quien es el amigo secreto
--- app.js
+++ app.js
@@ -1,1 +1,34 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+function agregarAmigo() {
+    let input = document.getElementById("amigo");
+    let nombre = input.value.trim();
+
+    if (nombre === "") {
+        alert("Por favor, ingresa un nombre válido.");
+        return;
+    }
+
+    let lista = document.getElementById("listaAmigos");
+    let nuevoElemento = document.createElement("li");
+    nuevoElemento.textContent = nombre;
+
+    lista.appendChild(nuevoElemento);
+    input.value = "";
+    input.focus();
+}
+
+function sortearAmigo() {
+    let lista = document.getElementById("listaAmigos").children;
+    
+    if (lista.length === 0) {
+        alert("No hay nombres en la lista para sortear");
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * lista.length);
+    
+    let nombreSorteado = lista[indiceAleatorio].textContent;
+
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<li>El amigo secreto es: <strong>${nombreSorteado}</strong></li>`;
+}
